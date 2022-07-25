4ec2b314f767b2aaf18917afbcd0a61362fa8674
Gabriel <gabegroenendaal@gmail.com>
2022-07-25 16:46:36 -0400

    Let's see if this fixes it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+config.action_cable.url = 'wss://ruckkus.herokuapp.com/cable'
+# config.web_socket_server_url = 'wss://ruckkus.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://ruckkus.herokuapp.com', 'https://ruckkus.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
